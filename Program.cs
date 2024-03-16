f1fcec889e1a6608d20e8abccf2dd4787d6dc4b7
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-15 22:26:14 -0300

    Resolve o exercício

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("--- Soletrar ---\n");
+Console.Write("Digite o texto a ser soletrado: ");
+
+string texto = Console.ReadLine()!;
+
+Console.WriteLine("\nSoletrando fica...\n");
+
+Console.WriteLine(
+    String.Join("-", texto.ToUpper().ToCharArray())
+);
+Console.WriteLine();
